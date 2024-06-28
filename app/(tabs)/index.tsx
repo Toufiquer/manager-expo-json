2ee435f51779bff567c2c32f71bb327bde691b4f
Toufiquer <toufiquerabc@gmail.com>
2024-06-29 04:00:08 +0600

    backup

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,50 @@
 import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-import { createData, getFile, readData, saveFile } from '@/components/store/store';
+import { Text, View } from "@/components/Themed";
+import { createData, storage } from "@/components/store/store";
 
 export default function TabOneScreen() {
-const data = { name: "John Doe", age: 33 };
-// saveFile(data, "userData.json"); // Save data to a file named userData.json
-createData(data)
-const handlePress = () => {
-  // getFile("userData.json").then((jsonData) => {
-  //   console.log("Retrieved data:", jsonData); // Print retrieved data from the file
-  // });
-  readData()
-
-}
+  const logSpace = () => {
+    console.log("");
+    console.log("");
+    console.log("");
+    console.log("");
+  };
+  const handlePressGet = async () => {
+    logSpace();
+    console.log("invoke fn handlePressGet Start");
+    await storage.getString("name");
+    console.log("invoke fn handlePressGet End ");
+  };
+  const handlePressSave = async () => {
+    logSpace();
+    console.log("invoke fn handlePressSave Start");
+    await storage.set("name", "new name 101");
+    console.log("invoke fn handlePressSave End");
+  };
+  const handlePressCreateData = async () => {
+    logSpace();
+    createData([]);
+  };
   return (
     <View style={styles.container}>
-      <Text className='text-rose-400'>Custom text</Text>
-      <TouchableOpacity onPress={handlePress}><Text>Get Data</Text></TouchableOpacity>
+      <Text className="text-rose-400">Custom text</Text>
+      <TouchableOpacity onPress={handlePressCreateData}>
+        <Text>Set []</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handlePressGet}>
+        <Text>Get Data</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={handlePressSave}>
+        <Text>Save Data</Text>
+      </TouchableOpacity>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
--- components/store/store.ts
+++ components/store/store.ts
@@ -1,23 +1,93 @@
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system'
 
-const data = { name: "John Doe", age: 30 };
-const filePath = FileSystem.documentDirectory + 'myData.json';
+type storeDataType = { name: string; data: any }
+const filePath = FileSystem.documentDirectory + 'myData.json'
 
+export const createData = async (data: any) => {
+    FileSystem.writeAsStringAsync(filePath, JSON.stringify(data), {
+        encoding: FileSystem.EncodingType.UTF8,
+    })
+}
 
-export const createData = async(data:any) =>{FileSystem.writeAsStringAsync(filePath, JSON.stringify(data), {
-  encoding: FileSystem.EncodingType.UTF8,
-});}
+export const readData = async () => {
+    try {
+        const filePath = FileSystem.documentDirectory + 'myData.json'
+        const fileData = await FileSystem.readAsStringAsync(filePath)
+        const jsonData = JSON.parse(fileData)
 
-export const   readData=async()=> {
-  try {
-    const filePath = FileSystem.documentDirectory + 'myData.json';
-    const fileData = await FileSystem.readAsStringAsync(filePath);
-    const jsonData = JSON.parse(fileData);
+        console.log('Retrieved data:', jsonData)
 
-    console.log("Retrieved data:", jsonData); 
-
-    return jsonData; // You can use this data as needed
-  } catch (error) {
-    console.error("Error reading file:", error);
-  }
+        console.log('read file invoke')
+        return jsonData // You can use this data as needed
+    } catch (error) {
+        console.error('Error reading file:', error)
+        return error
+    }
+}
+export const storage = {
+    set: async (name: string, value: any) => {
+        console.log('invoke method set start')
+        let storeData = await readData()
+        console.log('found Data 1 ', storeData)
+        const isExist = storeData.find(
+            (data: storeDataType) => data.name === name
+        )
+        console.log('found Data 2 isExist', isExist)
+        if (isExist) {
+            console.log('found Data 3 isExist', isExist)
+            console.log('found Data 4 ', storeData)
+            storeData = storeData.map((curr: storeDataType) => {
+                const i = { ...curr }
+                if (i.name === name) {
+                    i.data = value
+                }
+                return i
+            })
+        } else {
+            console.log('found Data 5 isExist', isExist)
+            console.log('found Data 6 ', storeData)
+            storeData = [...storeData, { name, data: value }]
+        }
+        console.log('invoke method set End-1')
+        await createData(storeData)
+        console.log('invoke method set End-2')
+        return 'successfully created'
+    },
+    getString: async (name: string) => {
+        console.log('invoke method getString Start')
+        let storeData = await readData()
+        const isExist = storeData.find(
+            (data: storeDataType) => data.name === name
+        )
+        console.log('invoke method getString End')
+        return isExist
+    },
+    getNumber: async (name: string) => {
+        console.log('invoke method getNumber')
+        let storeData = await readData()
+        const isExist = storeData.find(
+            (data: storeDataType) => data.name === name
+        )
+        return isExist
+    },
+    clearAll: async () => {
+        console.log('invoke method clearAll')
+        await createData([])
+        return 'successfully deleted all'
+    },
+    delete: async (name: string) => {
+        console.log('invoke method delete')
+        let storeData = await readData()
+        storeData = storeData.filter(
+            (curr: storeDataType) => curr.name !== name
+        )
+        await createData(storeData)
+        return 'successfully deleted'
+    },
+    getAllKeys: async () => {
+        console.log('invoke method getAllKeys')
+        let storeData = await readData()
+        const result = storeData.map((curr: storeDataType) => curr.name)
+        return result
+    },
 }
