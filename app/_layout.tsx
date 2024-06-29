13b4795f9d58fa5375c0ba929777b04606fa1744
Toufiquer <toufiquerabc@gmail.com>
2024-06-30 00:37:41 +0600

    done @ inside layout first check user.menu if not found then fetch menu

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -54,7 +54,6 @@ export default function RootLayout() {
       if (!getUserMenu) {
         await getMenuAndSave();
       }
-      // console.log("getUserMenu : ", getUserMenu);
     })();
   }, []);
 
